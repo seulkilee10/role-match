65ae60cce3ddac00df30ec673484f9ebea2f3098
sklee <121122887+seulkilee10@users.noreply.github.com>
2024-08-26 16:05:49 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RoleMatchingApp from './RoleMatchingApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RoleMatchingApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
